02f311ed9613bb0870b0ec229f4a262f84b73c05
Basit Ali <opoali911@gmail.com>
2026-08-23 11:16:25 +0500

    fix and reinstall

--- src/components/ui/separator.jsx
+++ src/components/ui/separator.jsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils"
 function Separator({
   className,
   orientation = "horizontal",
+  
   ...props
 }) {
   return (
--- src/components/ui/skeleton.jsx
+++ src/components/ui/skeleton.jsx
@@ -10,6 +10,7 @@ function Skeleton({
       className={cn("animate-pulse rounded-md bg-muted", className)}
       {...props} />
   );
+  
 }
 
 export { Skeleton }
